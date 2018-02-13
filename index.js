7778220b66ade21a13b19c9fc3a0fb066baca58d
Anna <anna_prih@tut.by>
2018-02-13 09:13:51 +0300

    cwp-22-2

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by annae on 13.02.2018.
+ */
+const cwp_22 = require('@anna_prih/cwp_22_1_pr');
+
+console.log(cwp_22("15 9 + 10 - 14 - 7 * 5 14 * 14 - *"));
+console.log(cwp_22("8 2 15 * + 8 - 13 * 15 - 6 6 13 + + -"));
+console.log(cwp_22("4 9 + 1 - 2 6 13 8 0 6 9 * - - + + - *"));
+console.log(cwp_22("6 1 - 14 * 11 + 3 - 2 12 5 - * +"));
+console.log(cwp_22("2 11 6 * - 6 * 7 - 7 + 14 + 8 10 * +"));
+console.log(cwp_22("13 3 14 * 10 + * 12 4 + + 8 *"));
+console.log(cwp_22("10 3 11 - + 7 + 2 - 10 - 10 -"));
+console.log(cwp_22("4 9 - 1 14 - - 12 + 2 1 - - 8 -"));
+console.log(cwp_22("2 11 0 * * 13 15 - + 0 - 4 * 1 *"));
+console.log(cwp_22("4 8 6 10 * * * 0 11 * -"));
+
